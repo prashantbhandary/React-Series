1bd9756458bef013c5b26a5c0a8f9324184bdcc2
prashantbhandary <santhprashant@gmail.com>
2024-10-30 23:30:41 +0545

    cmt

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -2,5 +2,9 @@ import {useEffect , useState} from "react"
 
 
 function useCurrencyInfo(currency){
-    
+
+
+        useEffect(()=>{
+            fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{}.json`)
+        },[])
 }
